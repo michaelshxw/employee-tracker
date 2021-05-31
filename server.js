456cb94b885250e6bd0fb3a02e4435af3d7b8aca
michaelshxw <michaelshxw@gmail.com>
2021-05-31 21:17:02 +0800

    updated server.js with connection details, inquirer prompts, start of switch (to be finished), and functions for switch to use

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const sequelize = require('./config/connection');
+require('dotenv').config();
+const inquirer = require('inquirer');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+sequelize.sync().then(() => {
+  app.listen(PORT, () => start());
+}).catch(() => {
+  console.log("Error: cannot connect to server.");
+})
+
+function start() {
+  // welcome screen 
+  inquirer.prompt([
+    {
+      type: "list",
+      message: "What would you like to do?",
+      name: "userSelection",
+      choices: [
+        "View All Employees",
+        "View All Roles",
+        "View All Departments",
+        "Create New Employee",
+        "Create New Role",
+        "Create New Department",
+        "Update Employee Role"
+      ]
+    }
+  ]).then(function whichFunctionToRun(value) {
+    switch (value.userSelection) {
+      case "View All Employees":
+        viewAllEmployees();
+        break;
+      case "View All Roles":
+        viewAllRoles();
+        break;
+      default:
+        break;
+    }
+  })
+
+
+  // View All Employees 
+
+  function viewAllEmployees() {
+    // function data here
+  }
+  // View All Roles
+  // View All Departments
+
+  // Create New Employee
+  // Create New Role 
+  // Create New Department
+
+  // Update Employee Role
+
+};
